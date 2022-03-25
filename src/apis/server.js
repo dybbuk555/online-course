c6c8466697aeb0bce01dd83620521947487b749c
dybbuk555 <dybbuk555@gmail.com>
2022-03-25 21:12:12 +0800

    https

--- src/apis/server.js
+++ src/apis/server.js
@@ -2,7 +2,7 @@ import axios from "axios";
 const URL =
   process.env.NODE_ENV !== "production"
     ? "http://localhost:9000"
-    : "http://safe-plains-62599.herokuapp.com";
+    : "https://safe-plains-62599.herokuapp.com";
 
 export default axios.create({
   baseURL: URL,

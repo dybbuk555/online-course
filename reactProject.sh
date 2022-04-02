9f1cb704202be5c372436ff334573c032896aa88
dybbuk555 <dybbuk555@gmail.com>
2022-04-02 14:11:35 +0800

    demo

---
+++ reactProject.sh
@@ -0,0 +1,20 @@
+cd ./React_Udemy_Project/client/online_course
+start //B code . 
+start npm start
+cd C:/Users/USER/Desktop/nodejs/React_Udemy_Project/server
+sleep 0.5
+start //B code . 
+start nodemon
+cd C:/Users/USER/Desktop/nodejs/Udemy_WebDev/project/project7_LearningSystem/client
+sleep 0.5
+start //B code .
+cd C:/Users/USER/Desktop/nodejs/Udemy_WebDev/project/project7_LearningSystem/server
+sleep 0.5
+start //B code .
+cd C:/Users/USER/Desktop/nodejs/Udemy_WebDev_bootcamp/yelp/YelpCamp
+sleep 0.5
+start //B code .
+
+
+:: Get-Process -Id (Get-NetTCPConnection -LocalPort 3000).OwningProcess 
+:: taskkill /PID pidNumber

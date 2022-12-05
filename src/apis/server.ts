e83e4a7f38c966dc07a5eaf50561474eb7055fef
dybbuk555 <dybbuk555@gmail.com>
2022-12-05 22:11:20 +0000

    migrate from heroku to render

--- src/apis/server.ts
+++ src/apis/server.ts
@@ -2,7 +2,9 @@ import axios from "axios";
 const URL =
   process.env.NODE_ENV !== "production"
     ? "http://localhost:9000"
-    : "https://safe-plains-62599.herokuapp.com";
+    : "https://online-course-backend.onrender.com";
+    // migrate from heroku to render
+    //"https://safe-plains-62599.herokuapp.com";
 
 export default axios.create({
   baseURL: URL,
